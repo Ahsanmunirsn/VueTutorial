91aadca8c4d4221b9307e639c0badbf115070b33
Ahsan Munir <ahsanmalik.sn@gmail.com>
2020-12-17 16:47:24 +0500

    Speed Typer

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,81 @@
 <template>
   <div>
-    <h1>Typing Speed</h1>
-    <p>{{keywords.join(' | ') }}</p>
-    <input :value="inputValue" @keyup.space="processInput($event)">
+    <h1>Speed Typer</h1>
+    <p>
+      <span 
+        :class="{
+          correct: keyword.correct,
+          wrong: keyword.wrong,
+          pending: keyword.pending 
+          }"
+        :key="keyword.text" v-for="keyword in keywords">
+        {{ keyword.text }}{{' '}}</span>
+    </p>
+    <input type="text" :value="inputValue" 
+    @keyup.space="processInput($event)">
     
   </div>
 </template>
 
 <script>
+
+const defaultKeywords = ['template', 'data', 'javascript', 'angular', 'vue', 'react', 'python', 'movie', 'gym', 'football', 'software', 'engineer', 'university']
+.map(keyword =>{
+  return {
+    text: keyword,
+    correct: false,
+    wrong: false,
+    pending: true
+  }
+})
+     
+
 export default {
   data(){
     return {
       inputValue: "",
       index: 0,
-      keywords: [],
-    };
+      keywords: defaultKeywords,
+     };
   },
   methods: {
-    processInput(event){
-      const value = event.target.value;
-      this.keywords[this.index] = value;
-      this.inputValue = value;
+      processInput(event){  
+      const value = event.target.value.trim();
+
+      if(value === "") {
+        return;
+      }
+      if(this.keywords[this.index].text === value){
+        //correct answer
+        this.keywords[this.index].correct = true;
+        this.keywords[this.index].wrong = false;
+        this.keywords[this.index].wrong = false;
+      } else{
+        //wrong answer
+        this.keywords[this.index].correct = false;
+        this.keywords[this.index].wrong = true;
+        this.keywords[this.index].wrong = false;
+      }
+
+      this.inputValue = "";
       this.index++;
-    }
-  }
-  
+    },
+  },
 }; 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.pending {
+  font-weight: bold;
+}
+.correct{
+  font-weight: bold;
+  color: green;
+}
+.wrong{
+  font-weight: bold;
+  color: red;
+}
+
 </style>
